2b719ad74ce649eb74a05704f9d7e62319f64c8c
JulianKimmig <julian.kimmig@gmx.net>
2025-03-29 07:17:44 +0100

    Add rotation functionality to WebcamController and expose set_rotation method in WebcamWorker

--- src/funcnodes_webcam/webcam_worker.py
+++ src/funcnodes_webcam/webcam_worker.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Literal
 from funcnodes import (
     instance_nodefunction,
     NoValue,
@@ -119,3 +119,7 @@ class WebcamWorker(FuncNodesExternalWorker):
     async def stop(self):
         await self.stop_capture()
         return await super().stop()
+
+    @instance_nodefunction()
+    def set_rotation(self, rotation: Literal[0, 90, 180, 270] = 0):
+        self.controller.set_rotation(int(rotation))
